6d35c4c291fca11974f3a4037543c66ca7623b29
Germán Casares <german95n@gmail.com>
2018-02-23 19:01:05 +0100

    Finished step 6 & applied git flow from now on.

--- src/app/accounts/components/AccountsListView.vue
+++ src/app/accounts/components/AccountsListView.vue
@@ -2,20 +2,22 @@
   <div id="accounts-list-view">
     I'm a list of accounts!
 
-    <router-link :to="{ name: 'createEditAccount' }">Add an account</router-link>
+    <router-link :to="{ name: 'createAccount' }">Add an account</router-link>
 
     <ul>
       <li v-bind:key="no-template-key" v-for="(account, key) in accounts">
         {{ account.name }}
         <span class="tag is-small is-info">{{ categories[account.category] }}</span>
         ${{ account.balance }}
+        <a @click="confirmDeleteAccount(account)">Delete</a>
+        <router-link :to="{ name: 'editAccount', params: { accountId: account.id } }">Edit</router-link>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex';
+import { mapState, mapActions } from 'vuex';
 import { CATEGORIES } from '../../../consts';
 
 export default {
@@ -25,6 +27,25 @@ export default {
     return {
       categories: CATEGORIES
     };
+  },
+
+  mounted () {
+    this.loadAccounts();
+  },
+
+  methods: {
+    // this imports our vuex actions and maps them to methods on this component
+    ...mapActions([
+      'deleteAccount',
+      'loadAccounts'
+    ]),
+
+    confirmDeleteAccount (account) {
+      // note that these are backticks and not quotation marks
+      if (confirm(`Are you sure you want to delete ${account.name}?`)) {
+        this.deleteAccount(account);
+      }
+    }
   },
 
   computed: {
