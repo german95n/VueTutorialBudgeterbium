d83199e102f869901620244e1436a0872ec61ee3
Germán Casares <german95n@gmail.com>
2018-02-24 10:00:18 +0100

    Finished step 7 with the refactor to the code.

--- src/app/accounts/routes.js
+++ src/app/accounts/routes.js
@@ -3,17 +3,17 @@ import * as components from './components';
 export default [
   {
     path: '/',
-    component: components.AccountsListView,
+    component: components.AccountsList,
     name: 'accountsListView'
   },
   {
     path: '/accounts/create',
-    component: components.CreateEditAccount,
+    component: components.CreateUpdateAccount,
     name: 'createAccount' // note that we changed this since we are using separate routes for create and edit
   },
   {
-    path: '/accounts/:accountId/edit', // the URL accepts an accountId parameter
-    component: components.CreateEditAccount,
-    name: 'editAccount'
+    path: '/accounts/:accountId/update', // the URL accepts an accountId parameter
+    component: components.CreateUpdateAccount,
+    name: 'updateAccount'
   }
 ];
--- src/app/accounts/vuex/actions.js
+++ src/app/accounts/vuex/actions.js
@@ -1,10 +1,10 @@
 import { guid } from '../../../utils';
-import { removeAccount, saveAccount, fetchAccounts } from '../api';
+import { deleteAccount as deleteAccountFromAPI, saveAccount, fetchAccounts } from '../api';
 
-export const addAccount = ({ commit }, data) => {
+export const createAccount = ({ commit }, data) => {
   let id = guid();
   let account = Object.assign({ id: id }, data); // copy the data into a new object with the generated ID
-  commit('ADD_ACCOUNT', {account: account});
+  commit('CREATE_ACCOUNT', {account: account});
   saveAccount(account).then((value) => {
     // we've saved the account, what now?
   });
@@ -17,7 +17,7 @@ export const updateAccount = ({ commit }, data) => {
 
 export const deleteAccount = ({ commit }, data) => {
   commit('DELETE_ACCOUNT', { account: data });
-  removeAccount(data);
+  deleteAccountFromAPI(data);
 };
 
 export const loadAccounts = (state) => {
@@ -25,9 +25,7 @@ export const loadAccounts = (state) => {
   // later we might want to be able to force reload them
   if (!state.accounts || Object.keys(state.accounts).length === 0) {
     return fetchAccounts().then((res) => {
-      let accounts = {};
-      Object.keys(res).forEach((key) => { accounts[res[key].id] = res[key]; });
-      state.commit('LOAD_ACCOUNTS', accounts);
+      state.commit('LOAD_ACCOUNTS', res);
     });
   }
 };
